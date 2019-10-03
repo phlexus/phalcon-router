515f48ce777ea5c52fe17bc3d7c9d7b65a414bde
Anton Vasiliev <aionbase@gmail.com>
2019-10-03 16:54:43 +0100

    Create RouterFiller class skeleton

---
+++ src/RouterFiller.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+class RouterFiller
+{
+    /**
+     * @var array
+     */
+    protected $routes;
+
+    public function __construct(array $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function fill(): void
+    {
+        // TODO
+    }
+}
